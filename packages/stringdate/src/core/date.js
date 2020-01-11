67416e068031d230a28e7eed0179f7b481088cb4
Allan Hortle <allanhortle@gmail.com>
2020-01-12 10:26:06 +1100

    Remove timezone before processing and treat everything as UTC

--- packages/stringdate/src/core/date.js
+++ packages/stringdate/src/core/date.js
@@ -4,10 +4,27 @@ export function parseDate(dateString: string): Date {
     return new Date(dateString);
 }
 
-export function stringifyDate(date: Date): string {
-    return date.toISOString().split('T')[0];
+export function stringifyDate(date: Date, offset: string): string {
+    return stringifyDateTime(date, offset).split('T')[0];
 }
 
-export function stringifyDateTime(date: Date): string {
-    return date.toISOString();
+export function stringifyDateTime(date: Date, offset: string): string {
+    return date.toISOString().replace('Z', offset || 'Z');
+    //const tz = date.getTimezoneOffset();
+    //const tzMinutes = tz % 60;
+    //let timezone = 'Z';
+
+    //if(tz !== 0) {
+        //timezone = (tz > 0 ? '-' : '+') + pad(parseInt(Math.abs(tz / 60)))
+            //+ (tzMinutes !== 0 ? pad(tzMinutes) : '00');
+    //}
+
+    //return pad(date.getFullYear())
+        //+ '-' + pad(date.getMonth() + 1)
+        //+ '-' + pad(date.getDate())
+        //+ 'T' + pad(date.getHours())
+        //+ ':' + pad(date.getMinutes())
+        //+ ':' + pad(date.getSeconds())
+        //+ '.' + pad(date.getMilliseconds(), 3)
+        //+ timezone;
 }
--- packages/stringdate/src/core/wrap.js
+++ packages/stringdate/src/core/wrap.js
@@ -7,11 +7,19 @@ export default function wrapFunction<A>(fn: A) {
         // flags
         const isDuration = input.indexOf('P') !== -1;
         const isTime = input.indexOf('T') !== -1;
+        const isUTC = !isTime || input.indexOf('Z') !== -1;
+        let offset = null;
+        if(isTime && !isUTC) {
+            input = input.replace(/(T.*?)([+\-].*)$/, (_, time, zone) => {
+                offset = zone;
+                return time + 'Z';
+            });
+        }
 
         // compute the next value
         let nextValue = fn(
-            isDuration ? parseDuration(input) : parseDate(input),
-            {isDuration, isTime}
+            isDuration ? parseDuration(input) : parseDate(isTime ? input : input + 'T00:00:00.000Z'),
+            {isDuration, isTime, isUTC}
         );
 
         // convert durations to string
@@ -21,8 +29,8 @@ export default function wrapFunction<A>(fn: A) {
 
         // convert dates to string based on exisiting input
         return isTime
-            ? stringifyDateTime(nextValue)
-            : stringifyDate(nextValue)
+            ? stringifyDateTime(nextValue, offset)
+            : stringifyDate(nextValue, offset)
         ;
     };
 }
